6a84dcb259dd5fd6383432a7bcf7a37e1f7c177a
exyyyl <shoxa0010006@gmail.com>
2025-12-03 13:43:26 +0800

    fix main route

--- components/headerMenu.tsx
+++ components/headerMenu.tsx
@@ -9,12 +9,12 @@ export default function HeaderMenu() {
     return (
         <div className="nav-bar">
             <div className="logo">
-                <a href="/home"><img src="https://images.squarespace-cdn.com/content/v1/673ecb02839de7577f9c0982/9ab44559-e4c0-42d3-b18e-0379f4490006/ADHOC_LOGO_FULL_SMALLWhite.png?format=1500w" height="50px"/></a>
+                <a href="/"><img src="https://images.squarespace-cdn.com/content/v1/673ecb02839de7577f9c0982/9ab44559-e4c0-42d3-b18e-0379f4490006/ADHOC_LOGO_FULL_SMALLWhite.png?format=1500w" height="50px"/></a>
                 <label className="version">A.//0.1v</label>
             </div>
             <div className="menu-right">
                 <ul className="background">
-                    <li><a href="/home" className="active">Main</a></li>
+                    <li><a href="/" className="active">Main</a></li>
                     <li><a href="/jobs">Jobs</a></li>
                     <li><a href="/about">About</a></li>
                     <li><a href="/contact">Contact</a></li>
