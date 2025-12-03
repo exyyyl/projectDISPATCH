575b57668f840e9e2dfc773a08235725aec78487
exyyyl <shoxa0010006@gmail.com>
2025-12-03 13:39:17 +0800

    hotfix

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,5 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-const isProd = process.env.NODE_ENV === 'production';
-const basePath = isProd ? '/projectDISPATCH' : '';
-module.exports = {  
-  output: 'export',
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : '',
-  };
 
 export default nextConfig;
